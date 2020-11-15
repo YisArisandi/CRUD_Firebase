2f5e92537f52b5d7d3ab811013a2cdd959b1bbdc
YisArisandi <72584663+YisArisandi@users.noreply.github.com>
2020-11-15 22:01:26 +0700

    crudFirebass

---
+++ app/src/main/java/com/example/crud_firebase/MyFriendsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.crud_firebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyFriendsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_friends)
+    }
+}
